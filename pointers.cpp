8449e826f9621667254d4e3187697cc316f3d3a9
Akash Verma <aakashverma4102@gmail.com>
2022-06-18 23:36:39 +0530

    pointers

---
+++ pointers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int arr[10];
+    // cout<<*arr<<endl;
+    // cout<<arr[0]<<endl;
+    // int i=9 ;
+    // cout<<i[arr]<<endl;
+    // cout<<arr[i]<<endl;
+
+    // char ch[6]="abcde";
+    // cout<<ch<<endl;
+    // char *c=&ch[0];
+    // cout<<c<<endl;
+    // char temp='z';
+    // char *p=&temp;
+    // cout<<p<<endl;
+
+     
+    return 0;
+}
